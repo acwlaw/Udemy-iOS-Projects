c85831d8ca32c059ab8f1959d8ecc2e107317f74
Alex Law <alexlaw@Alexs-MBP-4.hitronhub.home>
2017-12-21 22:45:13 -0700

    linked first segue

--- Swoosh App/swoosh-app/swoosh-app/ViewController.swift
+++ Swoosh App/swoosh-app/swoosh-app/ViewController.swift
@@ -11,21 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var welcomeBG: UIImageView!
-    @IBOutlet weak var titleTextLabel: UILabel!
-    @IBOutlet weak var topTitleLabel: UILabel!
-    @IBOutlet weak var bottomTitleLabel: UILabel!
     @IBOutlet weak var swooshLogo: UIImageView!
-    @IBAction func startButton(_ sender: Any) {
-    }
-    override func viewDidLoad() {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // code that is called when we call the back button
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
     }
-
-
 }
 
