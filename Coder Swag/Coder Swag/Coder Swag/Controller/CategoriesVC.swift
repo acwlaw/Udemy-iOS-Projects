9aff2454a569316db51f508e1ccf5042a8697da6
Alex Law <alexlaw@Alexs-MacBook-Pro-4.local>
2017-12-29 11:50:04 -0700

    Prepared TableView

---
+++ Coder Swag/Coder Swag/Coder Swag/Controller/CategoriesVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Coder Swag
+//
+//  Created by Alex Law on 2017-12-27.
+//  Copyright © 2017 alex. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    
+    @IBOutlet weak var categoryTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
